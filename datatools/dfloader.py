ffc0e1a0b0fe6a5d787890d8558de48d2dc8fab6
KrishnaMohan <krishnamohan-seelam@users.noreply.github.com>
2018-06-04 09:13:21 +0530

    modified

--- datatools/dfloader.py
+++ datatools/dfloader.py
@@ -25,29 +25,48 @@ class DataFrameLoader():
     @property
     def dataframe(self):
         return self.__dataframe
-
+     
     @property
     def categorical_features(self):
-        return self.__categorical_features
+        """ returns categorical features of dataframe as popo list.
+            popo - plain old python object
+        """
+        return list(self.__categorical_features)
 
     @property
     def continuous_features(self):
-        return self.__continuous_features
+        """ returns continuous features of dataframe as popo list.
+        """
+        return list(self.__continuous_features)
 
     @property
-    def numerical_dataframe(self):
+    def continuous_dataframe(self):
+        """ returns dataframe based on continuous_features.
+        """
         return self.__dataframe[self.continuous_features]
 
     @property
     def categorical_dataframe(self):
+        """ returns dataframe based on categorical_features.
+        """
         return self.__dataframe[self.categorical_features]
 
     def load_data(self, *args, **kwargs):
+        """ invokes respective load method based on extension.
+        Args:
+        *args:
+        **kwargs:
+        """
         _, extension = splitext(args[0])
         if extension[1:].lower() == 'csv':
             return self.load_csv(*args, **kwargs)
 
     def load_csv(self, *args, **kwargs):
+        """ returns panda dataframe  using *args, **kwargs .
+        Args:
+        *args:
+        **kwargs:
+        """
         dataframe = pd.DataFrame()
         try:
             dataframe = pd.read_csv(*args, **kwargs)
@@ -70,7 +89,7 @@ def main():
     print(loader.categorical_features)
     print(loader.continuous_features)
     print(loader.categorical_dataframe.head())
-    print(loader.numerical_dataframe.head())
+    print(loader.continuous_dataframe.head())
 
 
 if __name__ == '__main__':
